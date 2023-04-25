e259eb3c785ac6fb4e22384b33f9597fda3882c2
max_desktop <maxwell.adelman@gmail.com>
2023-04-25 19:49:22 -0400

    added new cpp file for 15b

---
+++ Adelman_TemplatesAndExceptions/Adelman_Templates.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//Maxwell Adelman
+//4/25/2023
+//CIS 1202.501
+//Exceptions assignment 15a
+
+int main() {
+
+}
