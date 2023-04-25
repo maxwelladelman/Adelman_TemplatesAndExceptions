0b8f039487c4883e655f9fd28847539b6b48e5fa
max_desktop <maxwell.adelman@gmail.com>
2023-04-25 15:43:27 -0400

    Modified Exeptions file (15a)

--- Adelman_TemplatesAndExceptions/Adelman_Exceptions.cpp
+++ Adelman_TemplatesAndExceptions/Adelman_Exceptions.cpp
@@ -5,20 +5,89 @@
 using namespace std;
 
 //Maxwell Adelman
-//__/__/2022
+//4/25/2023
 //CIS 1202.501
-//  assignment
+//Exceptions assignment 15a
+char character(char start, int offset);
 int main() {
 
+	char temp;
+	try {
+		temp = character('a', 1);
+		cout << temp << endl;
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+	}
+	
+
+	try {
+		temp = character('Z', -1);
+		cout << temp << endl;
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+	}
+
+	try {
+		temp = character('Z', 3);
+		cout << temp << endl;
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+	}
 
 
 
 
 
-
+	return 0;
 
 
 
 
 
  }
+
+
+
+char character(char start, int offset) {
+	//check if start is a letter
+	if (!isalpha(start)) {
+		throw invalid_argument("Invalid Character Exception");
+
+	}
+	//check if offset is inherently out of bounds
+	if (offset > 26) {
+		throw invalid_argument("Invalid Range Exception");
+	}
+	
+	//add offset
+	char temp;
+	temp = start + offset;
+
+
+	if (islower(start)) {
+
+		
+		if (!isalpha(temp) || isupper(temp)) {
+			throw invalid_argument("Invalid Range Exception");
+		}
+		else {
+			return temp;
+		}
+
+	}
+	if (isupper(start)) {
+
+
+		if (!isalpha(temp)) {
+			throw invalid_argument("Invalid Range Exception");
+		}
+		else {
+			return temp;
+		}
+
+	}
+
+}
