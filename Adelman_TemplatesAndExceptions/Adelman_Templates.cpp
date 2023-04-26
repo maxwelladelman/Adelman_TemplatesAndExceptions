3da8035bebb95f378622824d26fb67a8f4e48126
max_desktop <maxwell.adelman@gmail.com>
2023-04-25 20:26:30 -0400

    Modified 15b

---
+++ Adelman_TemplatesAndExceptions/Adelman_Templates.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//Maxwell Adelman
+//4/25/2023
+//CIS 1202.501
+//Template assignment 15b
+template <class T>
+T half(T num);
+
+int main() {
+
+
+
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+
+}
+
+
+
+template <class T>
+T half(T num) {
+	T quotient = (double)(num / 2.0);
+	//check if integer, if so apply rounding if 0.5 or over
+	if (is_integral<T>::value && quotient >= 0.5) {
+		return static_cast<T>(quotient) + 1;
+
+	}
+
+	return static_cast<T>(quotient);
+}
